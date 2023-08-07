fbb17df2237a9c4d51499d780646152031837dda
unknown <flowerrosa76@gmail.com>
2023-08-07 17:25:24 +0900

    new version

--- script.js
+++ script.js
@@ -17,3 +17,26 @@ function navSlide () {
   })
 }
 
+
+let toTopButton = document.getElementById("toTopButton");
+// let contactsContainer = document.querySelector(".contactActions");
+
+
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTopButton.style.display = "block";
+    // contactsContainer.style.display = "block";
+  } else {
+    toTopButton.style.display = "none";
+    // contactsContainer.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
